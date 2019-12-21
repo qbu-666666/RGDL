361c5a91d28292de683a1b70612e74eecbd62fa9
2147483647 <54267630+qbu-666666@users.noreply.github.com>
2019-12-21 20:25:20 +0800

    RGDL-0
    The third update.

--- RGDL-0/RGDL-batch_generate.cpp
+++ RGDL-0/RGDL-batch_generate.cpp
@@ -1,11 +1,7 @@
 namespace RGDL{
     template<typename mainfunc>
-    void batch_generate::batch_generate_data(char** file_name,int n,mainfunc mainfunction){
+    void batch_generate_data(char** file_name,int n,mainfunc function){
         std::ofstream of;
-        for(int i=0;i<n;++i){
-            of.open(file_name[i]);
-            mainfunction.run();
-            of.close();
-        }
+        for(int i=0;i<n;++i) of.open(file_name[i]),function.rnu(),of.close();
     }
 }
